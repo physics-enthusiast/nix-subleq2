5e006a5718171e7a6f45dc6e1ba17d15b594218c
Physics Enthusiast <118960463+physics-enthusiast@users.noreply.github.com>
2024-06-18 15:20:41 +0800

    Update subleq2.nix

--- subleq2.nix
+++ subleq2.nix
@@ -1,3 +1,18 @@
+{ mem
+# number of "clock cycles" to run for
+, iterCount ? 1000
+# length of a word, defaults to 32 bits
+, width ? 32
+# value the accumulator is initialize to
+, acc ? 0
+# initial instruction pointer position
+, ptr ? 0
+# function that takes the a pointer to a, checks if you jumped
+# to a magic address, does stuff to if you did, and optionally
+# "halts" the program
+# defaults to "no addresses are special, program never halts"
+, magic ? (ptr: mem: mem)
+}@init:
 let
   # maximum unsigned integer size
   wordsize = width:
@@ -8,27 +23,21 @@ let
   # subtract the integer representations of 2 words
   subtract = a: b: width:
     wrap width (a + (wordsize width - b));
-in
-step = state: iter:
-  let
+  step = state: iter: if mem.halts or false then
+    state
+  else let
+    inherit (state) magic width;
     # addresses are integers which must be coerced into strings
     # before we can index an attrset with them
     deref = addr:
       # memory is initialized to zeros
       state.mem."${builtins.toString addr}" or 0;
-    # defaults to 32 bit word width
-    width = state.width or 32;
-    # function that takes the a pointer to a, checks if you jumped
-    # to a magic address, does stuff to if you did, and optionally
-    # "halts" the program
-    # defaults to "no addresses are special, program never halts"
-    magic = state.magic or (ptr: mem: mem);
     # acc = a - acc
     # ptr is **a so we must dereference twice
     acc = subtract (deref (deref state.ptr)) (state.acc or 0) width;
     # we test if acc is negative under the two's complement
     # representation by comparing it to the maximum positive integer
-    ptr = if acc > (wordsize width -1) then
+    ptr = if (acc > (wordsize width -1)) or (acc == 0) then
       # jump to b
       deref (state.ptr + 1)
     else
@@ -38,7 +47,6 @@ step = state: iter:
       { name = builtins.toString (deref state.ptr); value = acc; }
     ]));
   in
-    if mem.halts or false then
-      state // { mem = state.mem // { halts = true; }; }
-    else
-      { inherit acc magic mem ptr width; }
+    { inherit acc iter magic mem ptr width; }
+in
+  builtins.foldl' step init (builtins.genList (n: n) iterCount)
